2e11fa084cb9b342b8722c3fe435bf723b9811b4
theDataGeek <msarkar@bayshoreintelligence.com>
2020-03-13 12:12:53 +0530

    Added the live prediction

--- src/engine/inference.py
+++ src/engine/inference.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import yfinance as yf
 from config import Config
 import click
@@ -7,7 +6,7 @@ import silence_tensorflow
 import tensorflow.compat.v1 as tf
 from utils import get_tensorflow_config
 from data import StockDataset
-
+from train import Train
 
 def _get_correct_checkpoint(root, checkpoint):
     ckptPath = root.joinpath("checkpoints")
@@ -70,13 +69,14 @@ def main(config, checkpoint):
     # Prepare for the model
     X_test, y_test = StockDataset.prepare_for_test_single(data)
 
-    print(X_test.shape)
-    print(y_test.shape)
+    # print(X_test.shape)
+    # print(y_test.shape)
 
     conf = Config(config)
     root = conf.root
     ckptPath = _get_correct_checkpoint(root, checkpoint)
 
+    print(f"Using checkPoint {ckptPath.name}")
     if ckptPath:
         # Get meta file to restore the graph
         metaFile = _get_meta_file(ckptPath)
@@ -85,24 +85,33 @@ def main(config, checkpoint):
         with tf.Session(config=gconf) as sess:
             saver = tf.train.import_meta_graph(str(metaFile))
             saver.restore(sess, tf.train.latest_checkpoint(str(ckptPath)))
+            sess.run(tf.global_variables_initializer())
+            graph = tf.get_default_graph()
+            print("Model restored!!")
 
             # Get all the graph placeholders, namely, X, y, keep_prob
-            ts, iS = conf.cell_dim
-            inputs = tf.placeholder(tf.float32,
-                                    [1, ts, iS],
-                                    name="inputs")
-            targets = tf.placeholder(tf.float32, [1, iS],
-                                     name="targets")
-            kp = tf.placeholder(tf.float32, None, name="keep_prob")
+            # ts, iS = conf.cell_dim
+            # inputs = tf.placeholder(tf.float32,
+            #                         [1, ts, iS],
+            #                         name="inputs")
+            # targets = tf.placeholder(tf.float32, [1, iS],
+            #                          name="targets")
+            # kp = tf.placeholder(tf.float32, None, name="keep_prob")
 
+            inputs = graph.get_tensor_by_name("inputs:0")
+            targets = graph.get_tensor_by_name("targets:0")
+            kp = graph.get_tensor_by_name("keep_prob:0")
+
+            output = graph.get_tensor_by_name("Linear/output:0")
+            loss = Train.squared_loss(output, targets, "test_loss")
             # Prepate the feed dict
             test_data_feed = {
                 inputs: X_test,
                 targets: y_test,
                 kp: 1.0
             }
-
-            test_loss, test_pred = sess.run(feed_dict=test_data_feed)
+            #
+            test_loss, test_pred = sess.run([loss, output], feed_dict=test_data_feed)
             print(test_loss)
             print(test_pred)
 
